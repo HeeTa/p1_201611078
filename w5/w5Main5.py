96021de0b82460dacd3a6ebee8aed4545a15f504
HeeTa <sdknm2@gmail.com>
2016-03-30 15:31:49 +0900

    Grading program
    input score, get grade

---
+++ w5/w5Main5.py
@@ -0,0 +1,23 @@
+﻿score=raw_input("Input Score(0~100: ")
+def check(val):
+	try:
+		float(val)
+	except ValueError:
+		print "wrong format! Plese Input Float number!"
+		raw_input("input any key to exit")
+		exit()
+
+check(score)
+score=float(score)
+if 90<score<=100:
+    grade="A"
+elif 80<score<=90:
+    grade="B"
+elif 70<score<=80:
+    grade="C"
+elif 60<score<=80:
+    grade="D"
+else:
+    grade="F"
+print "Grade is %s"%grade
+raw_input("input any key to exit")
