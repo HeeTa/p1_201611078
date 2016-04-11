94a1b43e816f693786c6b3eaf50f5a76e4402f3b
Hee Chan Shin (신희찬) 201611078 <sdknm2@gmail.com>
2016-04-11 14:36:16 +0900

    sum of the list

--- w6/list test.py
+++ w6/list test.py
@@ -1,8 +1,9 @@
-﻿y=list()
+y=list()
+for i in range(1,1000):
+	if i%4==0 and i%5!=0:
+		y.append(i)
+#y라는 list를 정의합니다.	
 def sumList(y):
-	for i in range(1,1000):
-		if i%4==0 and i%5!=0:
-			y.append(i)
 	sum=0
 	for i in range(len(y)):
 		sum=sum+y[i]	
