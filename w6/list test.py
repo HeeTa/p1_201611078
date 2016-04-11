5e5387cf1e458a75587726b4e9dd18d1397aaac6
Hee Chan Shin (신희찬) 201611078 <sdknm2@gmail.com>
2016-04-11 13:53:50 +0900

    sumList

---
+++ w6/list test.py
@@ -0,0 +1,13 @@
+﻿y=list()
+def sumList(y):
+	for i in range(1,1000):
+		if i%4==0 and i%5!=0:
+			y.append(i)
+	sum=0
+	for i in range(len(y)):
+		sum=sum+y[i]	
+	return sum
+def main():
+	print sumList(y)
+main()
+raw_input("enter to exit")
